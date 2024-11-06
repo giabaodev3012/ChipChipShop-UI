392212dba29d8625e09c89c09a15acafb9714253
Nguyen Luong Gia Bao <nguyenluonggiabao301203@gmail.com>
2024-11-06 10:45:04 +0700

    Update .gitignore

---
+++ ChipChipShopSolution.AdminApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChipChipShopSolution.AdminApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
